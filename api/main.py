ac9f9a0fb64ef046db30c6213d33f5023df0a82a
2noScript <dev.2noscript@mail.com>
2023-11-05 20:23:54 +0700

    [Modify] setup deploy to vercel

--- api/main.py
+++ api/main.py
@@ -1,6 +1,27 @@
 from flask import Flask,request,jsonify
-from useragent import getUserAgent
-from browser_headers import getBrowserHeaders
+from fake_headers import Headers
+from fake_useragent  import UserAgent
+
+def getBrowserHeaders(limit):
+    headersList=[]
+    header = Headers(
+        # generate any browser & os headeers
+        headers=False  # don`t generate misc headers
+    )
+    for i in range(limit):
+        headersList.append(header.generate())
+    return headersList
+
+def getUserAgent(limit):
+    userList=[]
+    ua = UserAgent()
+
+    for i in range(limit):
+        userList.append(ua.random)
+    return userList
+
+
+
 
 
 app=Flask(__name__)
--- api/useragent.py
+++
@@ -1,9 +0,0 @@
-from fake_useragent  import UserAgent
-
-def getUserAgent(limit):
-    userList=[]
-    ua = UserAgent()
-
-    for i in range(limit):
-        userList.append(ua.random)
-    return userList
